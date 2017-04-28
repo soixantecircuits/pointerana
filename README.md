fb2241968180ed9fe3fabcdc599eb9248fa4cb32
gabrielstuff <gabriel@soixantecircuits.fr>
2017-04-29 00:37:47 +0200

    💎📘📦 first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Pointerana
+
+Pointerana got his name from pointer events API. This tool helps to track cursor event at the system level. It will observe click, movement and anything you want to track and send them to a [Graphana](https://grafana.com/) instance.
+
+## 🌍 Installation
+
+`yarn`
+
+## ⚙ Configuration
+
+You need to provide few config in order to send data to a graphana server.
+
+## 👋 Usage
+
+`yarn start`
+
+## 📦 Dependencies
+
+The project depends on:
+
+- iohook: [https://github.com/WilixLead/iohook/](https://github.com/WilixLead/iohook/)
+This lib is used to capture the pointer events.
+
+- request: [https://github.com/request/request]https://github.com/request/request()
+This lib allow to easily query the Graphana server
+
+- standard-settings: [https://github.com/soixantecircuits/standard-settings](https://github.com/soixantecircuits/standard-settings)
+We use this for settings management
+
+## 🕳 Troubleshooting
+
+This has been test on OSX(10.11.6) only for now
+
+## ❤️ Contribute
+
+Welcome! You've been far in the README. The first thing you can do is open an issue. Please do not be shy!
+If you feel motivate, you can open an issue, create a branch and ask for a pull request.
+
+Please follow the commit style of Soixante circuits. You can find it here: [https://github.com/soixantecircuits/standard/blob/master/commit.md](https://github.com/soixantecircuits/standard/blob/master/commit.md)
+
+Enjoy
---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const ioHook = require('iohook')
+const os = require('os')
+const request = require('request')
+const standardSettings = require('standard-settings')
+const settings = require('nconf').get()
+
+let url = settings.url
+
+ioHook.on('mousedown', function (msg) {console.log(msg);})
+
+ioHook.on('keypress', function (msg) {console.log(msg);})
+
+ioHook.on('keydown', function (msg) {console.log(msg);})
+
+ioHook.on('keyup', function (msg) {console.log(msg);})
+
+ioHook.on('mouseclick', function (msg) {
+  console.log(msg)
+  let payload = `click,city=zhengzhou,hostname=${os.hostname()},type=mouse value=1`
+  request.post({
+    url: url,
+    body: payload
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 204) {
+      console.log(body)
+    } else {
+      console.error(error)
+      console.log(response.statusCode)
+    }
+  })
+})
+
+ioHook.on('mousewheel', function (msg) {console.log(msg)})
+
+ioHook.on('mousemove', function (msg) {
+  console.log(msg)
+  let payload = `move,city=zhengzhou,hostname=${os.hostname()},type=mouse x=${msg.x},y=${msg.y}`
+  request.post({
+    url: url,
+    body: payload
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 204) {
+      console.log(body)
+    } else {
+      console.error(error)
+      console.log(response.statusCode)
+    }
+  })
+})
+
+ioHook.on('mousedrag', function (msg) {console.log(msg)})
+
+// start ioHook
+ioHook.start()
+
+console.log(`Hook started. We are sending to ${settings.graphana.url} with the city ${settings.graphana.city}.`)
